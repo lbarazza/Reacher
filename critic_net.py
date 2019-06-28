fb8e42df96bfea8ee9e62b73574b86e9a9898fc5
Leonardo Barazza <leonardo.barazza@gmail.com>
2019-06-28 23:11:28 +0200

    Update critic_net.py

--- critic_net.py
+++ critic_net.py
@@ -2,15 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 class Critic(nn.Module):
-    def __init__(self, nS, nA):
+    def __init__(self, nS, nA): # nS: state space size, nA: action space size
         super(Critic, self).__init__()
 
-        self.h1 = nn.Linear(nS + nA, 200)
-        self.h2 = nn.Linear(200, 300)
-        self.out = nn.Linear(300, 1)
+        self.h1 = nn.Linear(nS + nA, 100)
+        self.h2 = nn.Linear(100, 100)
+        self.out = nn.Linear(100, 1)
 
     def forward(self, s, a):
-        x = torch.cat((s, a), dim=1) #?
+        x = torch.cat((s, a), dim=1)
         x = F.relu(self.h1(x))
         x = F.relu(self.h2(x))
         x = self.out(x)
